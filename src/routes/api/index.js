456e459110352c1b37f8a67c106be069b75b7e9c
Colins <77634471+MDXCODE@users.noreply.github.com>
2024-03-30 18:53:24 -0400

    more route updates

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+//users
 router.get('/users', require('./users/getusers'));
 router.get('/users/:id', require('./users/getusersid'));
 router.post('/users', require('./users/postuser'));
 
+//tasks
+router.get('/tasks', require('./tasks/getalltasks'));
 router.get('/tasks/:id', require('./tasks/getusertasks'));
 
 module.exports = router;
